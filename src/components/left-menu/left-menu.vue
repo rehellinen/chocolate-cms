4d5ebf65b41cd8047f3687bc183e4bbc2288cc7c
rehellinen <rehellinen@foxmail.com>
2019-11-01 19:35:02 +0800

    left-menu

--- src/components/left-menu/left-menu.vue
+++ src/components/left-menu/left-menu.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
   div.container
+    router-link(to="/" tag="div")
+      .title
+        p rehellinen
     el-menu(
       default-active="0"
       class="el-menu-vertical-demo"
@@ -14,11 +17,9 @@
 </template>
 
 <script>
-import { MenuModel } from '../../model/MenuModel'
+import { Menu } from '../../model'
 import { Token } from '../../utils/Token'
 import config from '../../utils/config'
-
-const menu = new MenuModel()
 
 export default {
   data () {
@@ -27,12 +28,14 @@ export default {
     }
   },
   async created () {
-    const menus = await menu.getNormal()
+    const menus = await new Menu().getNormal()
     const type = new Token().getTypeFromCache()
-    if (type === config.ADMIN_TYPE.TEACHER) {
-      this.menu = menus.filter(item => config.TEACHER_PAGE.includes(item.url))
-    } else {
+    if (type === config.TOKEN_TYPE.SUPER) {
       this.menu = menus
+    } else {
+      // TODO: 权限系统的完善
+      this.menu = menus
+      // this.menu = menus.filter(item => config.ADMIN_PAGE.includes(item.url))
     }
   }
 }
@@ -41,9 +44,20 @@ export default {
 <style scoped lang="sass" rel="stylesheet/sass">
   @import "~sass/base"
   .container
-    height: 100%
     ul
       height: 100%
   .menu-title
-    margin-left: 15px
+    margin-left: 10px
+  .title
+    display: flex
+    justify-content: center
+    align-items: center
+    height: 80px
+    background-color: $theme-color
+    border-bottom: 1px solid $border-one
+    p
+      color: white
+      font-weight: bold
+      font-size: $biggest-font-size
+      letter-spacing: 2px
 </style>
