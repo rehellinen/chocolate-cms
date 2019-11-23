106bd96c7683dfd10531e7a2f37d28f777c71b55
rehellinen <rehellinen@foxmail.com>
2019-11-23 18:02:39 +0800

    全局引入element ui

--- .babelrc.js
+++ .babelrc.js
@@ -2,19 +2,12 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env', {
-        modules: false,
         useBuiltIns: 'usage',
         corejs: 2
       }
     ]
   ],
   plugins: [
-    '@babel/plugin-transform-runtime',
-    [
-      'component', {
-        libraryName: 'element-ui',
-        styleLibraryName: 'theme-chalk'
-      }
-    ]
+    '@babel/plugin-transform-runtime'
   ]
 }
--- src/index.js
+++ src/index.js
@@ -4,33 +4,15 @@
  *  Create On 2018/11/28 10:24
  */
 import Vue from 'vue'
-import App from './App'
-import { router } from './router/router'
-import store from './store'
-import {
-  Header, Container, Aside, Main, Footer,
-  Row, Col,
-  Menu, Submenu, MenuItemGroup, MenuItem,
-  Form, FormItem, Input, Button, Dialog, Select, Option,
-  Table, TableColumn,
-  Breadcrumb, BreadcrumbItem,
-  Upload, Card,
-  Pagination, DatePicker, Radio,
-  Progress
-} from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
 
-[
-  Header, Container, Aside, Main, Footer,
-  Row, Col,
-  Menu, Submenu, MenuItemGroup, MenuItem,
-  Form, FormItem, Input, Button, Dialog, Select, Option,
-  Table, TableColumn,
-  Breadcrumb, BreadcrumbItem,
-  Upload, Card,
-  Pagination, DatePicker, Radio,
-  Progress
-].forEach(item => Vue.use(item))
+import App from './App'
+import store from './store'
+import { router } from './router/router'
+
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
