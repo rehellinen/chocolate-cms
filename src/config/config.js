4b404cfeeb127c605c09f921d6cc84b770e7af28
rehellinen <rehellinen@foxmail.com>
2019-11-24 17:39:43 +0800

    配置项增加

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,8 @@ export default {
   restUrl: PRODUCTION
     ? 'http://127.0.0.1:9527'
     : 'http://127.0.0.1:9527',
+  // 需要导入的路由配置文件名称（放置目录为src/config/router）
+  ROUTER_CONF_FILES: ['framework', 'custom'],
   STATUS: {
     NORMAL: 1,
     DELETED: -1,
