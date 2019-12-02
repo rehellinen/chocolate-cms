a4147b6006788166076a5aaccfd731e0f63203b2
rehellinen <rehellinen@foxmail.com>
2019-12-02 09:47:53 +0800

    refactor(component): merge configs of card component

--- src/pages/index/config.js
+++ src/pages/index/config.js
@@ -1,13 +1,6 @@
-export const colorConf = [
-  'linear-gradient(230deg, #759bff, #843cf6)',
-  'linear-gradient(230deg, #fc5286, #fbaaa2)',
-  'linear-gradient(230deg, #ffc480, #ff763b)',
-  'linear-gradient(230deg, #0e4cfd, #6a8eff)'
-]
-
-export const iconConf = [
-  'el-icon-goods',
-  'el-icon-star-off',
-  'el-icon-user',
-  'el-icon-bell'
+export const cardConf = [
+  { color: 'linear-gradient(230deg, #759bff, #843cf6)', icon: 'el-icon-goods' },
+  { color: 'linear-gradient(230deg, #fc5286, #fbaaa2)', icon: 'el-icon-star-off' },
+  { color: 'linear-gradient(230deg, #ffc480, #ff763b)', icon: 'el-icon-user' },
+  { color: 'linear-gradient(230deg, #0e4cfd, #6a8eff)', icon: 'el-icon-bell' }
 ]
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,11 +3,12 @@
     el-row(:gutter="20" style="margin-bottom: 30px")
       el-col(:md="6" :sm="12" v-for="(item, index) in data" :key="index")
         card(
-        :title="item.title"
-        :num="item.num"
-        :text="item.text"
-        :icon="iconConf[index]"
-        :bgColor="colorConf[index]")
+          :title="item.title"
+          :num="item.num"
+          :text="item.text"
+          :icon="cardConf[index].icon"
+          :bgColor="cardConf[index].color"
+        )
     el-row.progress(:gutter="20")
       el-col(:md="12" :sm="24")
         progress-bar(:data="progress")
@@ -20,7 +21,7 @@ import { cmsMixin, progressMixin } from 'mixins'
 import Card from 'base/card/card'
 import ProgressBar from 'base/progress-bar/progress-bar'
 import { Home as Model } from 'model/Home'
-import { colorConf, iconConf } from './config'
+import { cardConf } from './config'
 
 export default {
   components: {
@@ -30,8 +31,7 @@ export default {
   mixins: [cmsMixin, progressMixin],
   data () {
     return {
-      colorConf: colorConf,
-      iconConf: iconConf
+      cardConf: cardConf
     }
   },
   methods: {
