c5764ae384467e75e8702b1def52fdecbe1bdf1f
rehellinen <rehellinen@foxmail.com>
2020-02-14 23:21:46 +0800

    feat: process with token

--- src/libs/utils/http.js
+++ src/libs/utils/http.js
@@ -1,10 +1,31 @@
 import axios from 'axios'
 import config from 'config'
+import Vue from 'vue'
+import { getAccessToken, getRefreshToken } from 'libs/utils/token'
 
 const processConfig = (config) => {
+  // 删除axios不需要的配置项
+  delete config.getAllResponse
 }
 
-const precessData = (data) => {
+const precessData = (config) => {
+  const data = config.data
+  return data
+}
+
+const processAuth = (config) => {
+  const getAuthHeader = (token) => `Bearer ${token}`
+  if (config.url === 'user/refresh') {
+    const refreshToken = getRefreshToken()
+    if (refreshToken) {
+      config.headers.Authorization = getAuthHeader(refreshToken)
+    }
+  } else {
+    const accessToken = getAccessToken()
+    if (accessToken) {
+      config.headers.Authorization = getAuthHeader(accessToken)
+    }
+  }
 }
 
 const http = axios.create({
@@ -17,26 +38,51 @@ const http = axios.create({
 http.interceptors.request.use(config => {
   // 增加容错率
   processConfig(config)
+  // 处理data字段
+  precessData(config)
   // 处理跨域的api，http://127.0.0.1:3000/
   if (config.url.indexOf('mock') === -1) {
     config.baseURL += '/api/'
   }
-  // 处理data字段
-  config.data = precessData(config.data)
+  processAuth(config)
   return config
 })
 
 http.interceptors.response.use(res => {
-  return res.data.data
+  const { status, data } = res
+  const error = (msg) => {
+    Vue.prototype.$message({
+      message: msg,
+      type: 'error'
+    })
+  }
+
+  // 处理参数错误的情况
+  if (status === 400) {
+    for (const [key, val] of Object.entries(data.data)) {
+      for (let msg of val) {
+        if (process.env.NODE_ENV !== 'production') {
+          msg = `${key} - ${msg}`
+        }
+        error(msg)
+      }
+    }
+    throw new Error(data.message)
+  }
+
+  return res
 })
 
-export const request = (url, method, data, otherConfig) => {
-  return http({
+export const request = async (url, method, data, config = {}) => {
+  // 该配置项表示是否返回所有的数据，为false时只返回res.data
+  const getAllResponse = config.getAllResponse
+  const res = await http({
     url,
     method,
     data,
-    ...otherConfig
+    ...config
   })
+  return getAllResponse ? res.data : res.data.data
 }
 
 export const get = (url, data, otherConfig) => {
