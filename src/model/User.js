a999b7d42ac490199f1f2cf214478ab58ee4c16a
rehellinen <rehellinen@foxmail.com>
2020-02-14 23:20:18 +0800

    improvement: add login method to user model

--- src/model/User.js
+++
@@ -1,9 +0,0 @@
-import { get } from 'libs/utils/http'
-import { BaseModel } from 'libs/model/BaseModel'
-
-export class User extends BaseModel {
-  // TODO: MOCK
-  static getMock () {
-    return get('user/all')
-  }
-}
