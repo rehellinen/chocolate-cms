34aee358e616dcf9b947ae12d2ab8240e85f701a
rehellinen <rehellinen@foxmail.com>
2020-02-14 23:22:49 +0800

    feat: add login method to user model

--- src/libs/model/User.js
+++ src/libs/model/User.js
@@ -1,13 +1,28 @@
-import { put } from 'libs/utils/http'
+import { get, post } from 'libs/utils/http'
+import { BaseModel } from 'libs/model/BaseModel'
+import { saveTokens } from 'libs/utils/token'
 
-export class User {
-  // 更改用户名
-  static editName () {
-    return put('user/name')
+export class User extends BaseModel {
+  // TODO: MOCK
+  static getMock () {
+    return get('user/all')
   }
 
-  // 更改用户头像
-  static editPortrait () {
-    return put('user/portrait')
+  /**
+   * 登录
+   * @param account 账号
+   * @param password 密码
+   */
+  static async getToken (account, password) {
+    const data = {
+      account,
+      password
+    }
+    const res = await post('/user/login', data, {
+      getAllResponse: true
+    })
+    console.log(res.data.accessToken)
+    saveTokens(res.data.accessToken, res.data.refreshToken)
+    return res
   }
 }
