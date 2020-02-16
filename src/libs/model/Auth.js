074cd5fc05c9f56589f576336fb57dc928a20206
rehellinen <rehellinen@foxmail.com>
2020-02-16 17:13:27 +0800

    feat: define Auth model

--- src/libs/model/Auth.js
+++ src/libs/model/Auth.js
@@ -1,70 +1,39 @@
 import { get, post } from 'libs/utils/http'
 import { BaseModel } from 'libs/model/BaseModel'
-import { saveTokens } from 'libs/utils/token'
 
-export class User extends BaseModel {
+export class Auth extends BaseModel {
   id
 
   name
 
-  account
-
-  password
-
-  avatar
+  bundle
 
   groupId
 
-  status
-
-  order
-
-
-  constructor (id, name, account,password, avatar, groupId, status, order) {
+  constructor (data) {
     super()
-    this.id = id
-    this.name = name
-    this.account = account
-    this.password = password
-    this.avatar = avatar
-    this.groupId = groupId
-    this.status = status
-    this.order = order
+    this.setData(data, [
+      'id',
+      'name',
+      'bundle',
+      'groupId'
+    ])
   }
 
   /**
-   * 登录
-   * @param account 账号
-   * @param password 密码
+   * 获取当前用户拥有的权限
    */
-  static async getToken (account, password) {
-    const data = {
-      account,
-      password
-    }
-    const res = await post('/user/login', data)
-    saveTokens(res.accessToken, res.refreshToken)
-    return res
+  static async getUserAuth () {
+    const auth = await get('/auth/self')
+    return new Auth(auth)
   }
 
   /**
-   * 获取已登录的账户信息
+   * 添加一条权限
+   * @param form 表单数据
    */
-  static async getUser () {
-    const user = await get('user/self')
-    return new User(
-      user.id,
-      user.name,
-      user.account,
-      user.password,
-      user.avatar,
-      user.groupId,
-      user.status,
-      user.order
-    )
-  }
-
-  static getAuth () {
-    const auth = await get('user/auth/self')
+  static async addAuth (form) {
+    const auth = new Auth(form)
+    return post('auth', auth)
   }
 }
