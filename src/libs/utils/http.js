7792cc85981418ece25a8a17425221e61d819690
rehellinen <rehellinen@foxmail.com>
2020-02-19 20:25:47 +0800

    refactor: add comments and simplified writing

--- src/libs/utils/http.js
+++ src/libs/utils/http.js
@@ -14,6 +14,7 @@ const processConfig = (reqConfig) => {
     reqConfig.method = 'GET'
   }
   reqConfig.method = reqConfig.method.toUpperCase()
+
   // headers默认值
   if (!reqConfig.headers) {
     reqConfig.headers = {}
@@ -21,25 +22,28 @@ const processConfig = (reqConfig) => {
   if (!reqConfig.headers['Content-Type']) {
     reqConfig.headers['Content-Type'] = 'application/json'
   }
+
   // GET方法下，data转为params
   if (reqConfig.method === 'GET') {
     if (!reqConfig.params) {
       reqConfig.params = reqConfig.data
     }
   }
+
   // 增加权限相关header字段
   const getAuthHeader = (token) => `Bearer ${token}`
   if (reqConfig.url === REFRESH_URL) {
     const refreshToken = getRefreshToken()
-    if (refreshToken) {
+    if (refreshToken !== 'undefined') {
       reqConfig.headers.Authorization = getAuthHeader(refreshToken)
     }
   } else {
     const accessToken = getAccessToken()
-    if (accessToken) {
+    if (accessToken !== 'undefined') {
       reqConfig.headers.Authorization = getAuthHeader(accessToken)
     }
   }
+  return reqConfig
 }
 
 const processResponse = async (res, allReqConfig) => {
@@ -53,8 +57,11 @@ const processResponse = async (res, allReqConfig) => {
   if (status >= 200 && status < 300) {
     return getResponseData(res)
   }
+  // 处理参数错误
   processParamsError(res)
+  // 处理refresh token过期
   processRefreshTokenError(res)
+  // 处理access token过期
   const newRes = await processAccessTokenError(res, allReqConfig)
   return getResponseData(newRes || res)
 }
@@ -96,23 +103,25 @@ const processRefreshTokenError = (response) => {
 const processAccessTokenError = async ({ status, data }, allReqConfig) => {
   // 令牌类型错误 / Token过期
   if (status === 20101 || status === 20102) {
-    Vue.prototype.$message({
-      message: data.message,
-      type: 'error'
-    })
-    // 刷新access_token
-    const res = await post(REFRESH_URL)
-    saveTokens(res.data.accessToken, res.data.refreshToken)
-
-    // 重发请求
     const reFetch = allReqConfig.otherConfig.reFetch !== false
     if (reFetch) {
-      allReqConfig.otherConfig.refetch = false
-      return request(allReqConfig.url,
-        allReqConfig.method,
-        allReqConfig.data,
-        allReqConfig.otherConfig
-      )
+      // 刷新access_token
+      const res = await post(REFRESH_URL)
+      saveTokens(res.data.accessToken, res.data.refreshToken)
+      // 重发请求
+      if (reFetch) {
+        allReqConfig.otherConfig.refetch = false
+        return request(allReqConfig.url,
+          allReqConfig.method,
+          allReqConfig.data,
+          allReqConfig.otherConfig
+        )
+      }
+    } else {
+      Vue.prototype.$message({
+        message: '登录状态已过期',
+        type: 'error'
+      })
     }
   }
 }
@@ -120,20 +129,28 @@ const processAccessTokenError = async ({ status, data }, allReqConfig) => {
 const http = axios.create({
   baseURL: config.BASE_URL,
   timeout: 5000,
-  // 所有响应情况都在拦截器中处理
   validateStatus: (status) => status >= 200 && status < 510
 })
 
 export const request = async (url, method, data, otherConfig = {}) => {
-  const allReqConfig = {
-    url,
-    method,
-    data,
-    ...otherConfig
+  try {
+    const allReqConfig = processConfig({
+      url,
+      method,
+      data
+    })
+
+    const response = await http(allReqConfig)
+
+    return await processResponse(response, {
+      url,
+      method,
+      data,
+      otherConfig
+    })
+  } catch (e) {
+    throw e
   }
-  processConfig(allReqConfig)
-  const response = await http(allReqConfig)
-  return processResponse(response, { url, method, data, otherConfig })
 }
 
 export const get = (url, data, otherConfig) => {
@@ -152,6 +169,7 @@ export const del = (url, data, otherConfig) => {
   return request(url, 'delete', data, otherConfig)
 }
 
+// TODO: file类型也在processConfig中处理
 export const uploadFile = (data, type) => {
   let configs = {
     headers: {
